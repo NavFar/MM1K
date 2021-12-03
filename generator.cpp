53ae80aaa5b2283ea0b6ea92c6954252e6eb2915
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 23:00:42 +0330

    flaten the load of jobs(i don't know why but results are more realistic

--- generator.cpp
+++ generator.cpp
@@ -5,23 +5,27 @@
 Generator::Generator(float lambda, float theta){
 	this->lambda = lambda;
 	this->theta = theta;
+	std::random_device rd;
+	this->randomNumberGenerator = std::default_random_engine(rd());
 }
 
 Job Generator::generateJob(Distribution dueTimeDistribution){
-	std::mt19937_64 randomNumberGenerator;
 	std::exponential_distribution<float> exponentialDistribution(this->lambda);
+	
 	float load = exponentialDistribution(randomNumberGenerator);
-	if(dueTimeDistribution == Distribution::Fixed)
+	load = 1;
+
+	if(dueTimeDistribution == Distribution::Fixed){
 		return Job(load,this->theta);
+	}
 	if(dueTimeDistribution == Distribution::Exponential){
 		exponentialDistribution = std::exponential_distribution<float>((double)1/this->theta);
 		float due = exponentialDistribution(randomNumberGenerator);
 		return Job(load,due);
 	}
-	return Job(1,1);
+	return   Job(1,1);
 }
 float Generator::generateJobInterval(){
-	std::mt19937_64 randomNumberGenerator;
 	std::exponential_distribution<float> exponentialDistribution(this->lambda);
 	return exponentialDistribution(randomNumberGenerator);	
 }
--- headers/generator.h
+++ headers/generator.h
@@ -3,10 +3,12 @@
 
 #include"job.h"
 #include"distribution.h"
+#include<random>
 class Generator{
 	private:
 		float lambda;
 		float theta;
+		std::default_random_engine randomNumberGenerator;
 	public:
 		Generator(float, float);
 		Job generateJob(Distribution);
