f004a02434757723ca66c884795e189dd0d85430
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 20:52:59 +0330

    add lambda and theta as part of generator values

--- headers/generator.h
+++ headers/generator.h
@@ -1,7 +1,11 @@
 #include"job.h"
 #include"distribution.h"
 class Generator{
+	private:
+		float lambda;
+		float theta;
 	public:
+		Generator(float, float);
 		Job generateJob(Distribution);
 		float generateJobInterval();
 };
