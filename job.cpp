ed0fc6d7e5cdf0e086c913db9b914b4cdaa3512a
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 22:58:27 +0330

    minor change

--- job.cpp
+++ job.cpp
@@ -1,11 +1,11 @@
 #include"headers/job.h"
-
+#include<iostream>
 Job::Job(float load, float due){
 	this->load = load;
 	this->due = due;
 }
 void Job::decreaseDue(float duration){
-	this->due -= duration;
+	this->due = this->due- duration;
 }
 float Job::getLoad(){
 	return this->load;
