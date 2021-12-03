345badad16bc2b7e071b9c35e9930a20e49c4a56
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 18:42:28 +0330

    fixed indention

--- headers/job.h
+++ headers/job.h
@@ -1,10 +1,11 @@
 class Job {
-	float load;
-	float due;
+	private:
+		float load;
+		float due;
 	public:
-	Job(float,float);
-	float getLoad();
-	void setLoad(float);
-	float getDue();
-	void setDue(float);
+		Job(float,float);
+		float getLoad();
+		void setLoad(float);
+		float getDue();
+		void setDue(float);
 }; 
