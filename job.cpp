0e1e00bd9243df833690de48b412dca1aa76dbf7
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 18:57:42 +0330

    add job implementation

---
+++ job.cpp
@@ -0,0 +1,19 @@
+#include"headers/job.h"
+
+Job::Job(float load, float due){
+	this->load = load;
+	this->due = due;
+}
+
+float Job::getLoad(){
+	return this->load;
+}
+void Job::setLoad(float load){
+	this->load = load;
+}
+float Job::getDue(){
+	return this->due;
+}
+void Job::setDue(float due){
+	this->due = due;
+}
