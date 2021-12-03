2346dfc90ea38a153e4bddd776a0d361bdf40c2c
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 22:58:57 +0330

    fix error related to removal of items in queue and bounding variables

--- server.cpp
+++ server.cpp
@@ -1,6 +1,5 @@
 #include"headers/server.h"
 #include"headers/job.h"
-
 Server::Server(float mu, int maxSize){
 	this->mu = mu;
 	this->maxSize = maxSize;
@@ -15,23 +14,28 @@ int Server::run(float duration){
 	int droped = 0;
 	while(duration>0 && this->queue.size()>0){
 		float passedTime=duration;
+		bool frontDone=false;
 		duration = duration - this->queue.front().getLoad();
-		if(duration>0){
+		if(duration>=0){
 			passedTime=this->queue.front().getLoad();
-			this->queue.pop_front();
+			frontDone=true;
 		}
 		else {
 			this->queue.front().setLoad(-1*duration);
 		}
 		std::deque<Job>::iterator it = this->queue.begin()+1;
-		while(it!= this->queue.end()){
+		while(it< this->queue.end()){
 			it->decreaseDue(passedTime);
-			if(it->getDue()<0){
+			if(it->getDue()<=0){
 				droped++;
 				it= this->queue.erase(it);
 			}
 			else
 				it++;
+		}
+		if(frontDone){
+
+			this->queue.pop_front();
 		}
 
 	}
