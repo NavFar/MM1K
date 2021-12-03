e9cc1d67256d9bae1868f550887e6494df1b9aaa
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 20:53:09 +0330

    implement generator

---
+++ generator.cpp
@@ -0,0 +1,26 @@
+#include"headers/generator.h"
+#include"headers/job.h"
+#include"headers/distribution.h"
+#include<random>
+Generator::Generator(float lambda, float theta){
+	this->lambda = lambda;
+	this->theta = theta;
+}
+
+Job Generator::generateJob(Distribution dueTimeDistribution){
+	std::mt19937_64 randomNumberGenerator;
+	std::exponential_distribution<float> exponentialDistribution(this->lambda);
+	float load = exponentialDistribution(randomNumberGenerator);
+	if(dueTimeDistribution == Distribution::Fixed)
+		return Job(load,this->theta);
+	if(dueTimeDistribution == Distribution::Exponential){
+		exponentialDistribution = std::exponential_distribution<float>((double)1/this->theta);
+		float due = exponentialDistribution(randomNumberGenerator);
+		return Job(load,due);
+	}
+}
+float Generator::generateJobInterval(){
+	std::mt19937_64 randomNumberGenerator;
+	std::exponential_distribution<float> exponentialDistribution(this->lambda);
+	return exponentialDistribution(randomNumberGenerator);	
+}
