6e9a0f6680ff021b14a6ced4f5b15be8c581be7b
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-03 18:48:18 +0330

    add server header file

---
+++ headers/server.h
@@ -0,0 +1,13 @@
+#include<deque>
+#include"job.h"
+
+class Server {
+	private:
+	std::deque<Job> queue;
+	float mu;
+	int maxSize;
+	public:
+	Server(float,int);
+	bool addJob(Job);
+	int run(float);
+}
