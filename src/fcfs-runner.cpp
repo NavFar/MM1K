1930abc77feb1d667551e4f101dbc7d4d09300aa
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-19 22:43:15 +0330

    minor change to make code more readable

--- src/fcfs-runner.cpp
+++ src/fcfs-runner.cpp
@@ -4,16 +4,17 @@
 
 int FCFSRunner::run(float duration, float mu,std::deque<Job>& queue){
 	int droped = 0;
-	while(duration>0 && queue.size()>0){
+	float load = duration * mu;
+	while(load>0 && queue.size()>0){
 		float passedTime=duration;
 		bool frontDone=false;
-		duration = duration - queue.front().getLoad();
-		if(duration>=0){
-			passedTime=queue.front().getLoad();
+		load = load - queue.front().getLoad();
+		if(load>=0){
+			passedTime=queue.front().getLoad()/mu;
 			frontDone=true;
 		}
 		else {
-			queue.front().setLoad(-1*duration);
+			queue.front().setLoad(-1*load);
 		}
 		std::deque<Job>::iterator it = queue.begin()+1;
 		while(it< queue.end()){
