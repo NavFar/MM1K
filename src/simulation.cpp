0245306b7462ffc2729bbfb00e12d725101c35dc
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-19 22:42:57 +0330

    get extra input to determine queue strategy

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,19 +1,32 @@
 #include<iostream>
 #include<fstream>
+#include<string>
 #include"generator.h"
 #include"server.h"
 #include"distribution.h"
 #include"job.h"
+#include"server-runner.h"
 #include"fcfs-runner.h"
+#include"ps-runner.h"
 #define REPEAT 10000000
 using namespace std;
 int main() {
 	cout.precision(20);
 	ifstream fin("parameters.conf");
 	float lambda=0,theta=0,mu=0;
+	string queueStrategy;
 	fin>>theta>>mu;
+	cin>>queueStrategy;
 	cin>>lambda;
 	Generator generator(lambda,theta);
+	ServerRunner * serverRunner;
+	
+	if(queueStrategy=="FCFS")
+		serverRunner = new FCFSRunner();
+	else if(queueStrategy=="PS")
+		serverRunner = new PSRunner();
+	else
+		return 1;
 	Server server(mu,12,new FCFSRunner());
 	long blocked=0;
 	long droped=0;
