fd4e789f249969625ec65beb7b51e0c01aee5225
navidfarahmand <navidfarahmand75@gmail.com>
2021-12-19 22:53:34 +0330

    add constant files to unify constants between both types

---
+++ headers/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+#define K 12  // number of slots in queue
+#define REPEAT 10000000  // how manytimes the simulation should repeat 
+#define PARAMETERS_FILE "parameters.conf"
+#define FCFS_NAME "FCFS"
+#define PS_NAME "PS"
+#endif
