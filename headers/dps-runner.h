d677cea3d83024de8e1bc8c57bac97f266abe2e5
navidfarahmand <navidfarahmand75@gmail.com>
2022-01-12 22:46:19 +0330

    remove unused method

--- headers/dps-runner.h
+++ headers/dps-runner.h
@@ -2,10 +2,13 @@
 #define DPS_RUNNER_H
 #include<deque>
 #include"job.h"
+#include"job-type.h"
 #include"server-runner.h"
 class DPSRunner :public ServerRunner {
         public:
 		std::deque<Job> run(float duration, float mu, std::deque<Job>& queue);
+	private:
+		std::deque<Job> runOneType(float duration, float mu, JobType jobType, std::deque<Job>& queue);
 };
 
 #endif
